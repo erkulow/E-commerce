d7d4b9f3a54c110b0aa105303ad3d7a1392e3715
erkulow <erkulow26@gmail.com>
2023-01-24 21:11:55 +0600

    config swipe slider

--- home/slider.js
+++ home/slider.js
@@ -1,12 +1,37 @@
-var swiper = new Swiper('.mySwiper', {
-	slidesPerView: 5,
-	spaceBetween: 30,
+const swiper = new Swiper('.mySwiper', {
+	slidesPerView: 7,
+	spaceBetween: 16,
 	pagination: {
 		el: '.swiper-pagination',
 		clickable: true,
 	},
-	autoplay: {
-		delay: 2500,
-		disableOnInteraction: false,
+	// autoplay: {
+	// 	delay: 2500,
+	// 	disableOnInteraction: false,
+	// },
+	breakpoints: {
+		// when window width is >= 320px
+		300: {
+			slidesPerView: 2,
+			spaceBetween: 12,
+		},
+		// when window width is >= 480px
+		560: {
+			slidesPerView: 3,
+			spaceBetween: 14,
+		},
+		// when window width is >= 640px
+		730: {
+			slidesPerView: 4,
+			spaceBetween: 16,
+		},
+		900: {
+			slidesPerView: 5,
+			spaceBetween: 16,
+		},
+		1000: {
+			slidesPerView: 6,
+			spaceBetween: 16,
+		},
 	},
 })
---
+++ media.js
@@ -0,0 +1,5 @@
+const desktop = window.matchMedia('(max-width: 1000px)')
+const tablet = window.matchMedia('(max-width: 728px)')
+const smallTablet = window.matchMedia('(max-width:580px)')
+const mobile = window.matchMedia('(max-width: 450px)')
+
