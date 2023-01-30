beb441fc78e431d1c3a51197f3447472d3a8d959
ulukmyrza2001 <ulukmyrzazhanybekov01@gmail.com>
2023-01-31 00:49:54 +0600

    feature menu

--- home/script.js
+++ home/script.js
@@ -2,9 +2,11 @@ const catalogBtn = document.getElementById('catalog')
 
 catalogBtn.addEventListener('click', () => {
 	const catalogContent = document.getElementById('catalog_wrapper')
-	if (catalogContent.classList.contains('catalog_wrapper_hide')) {
-		catalogContent.classList.remove('catalog_wrapper_hide')
-	} else {
+	if (catalogContent.classList.contains('catalog_wrapper_show')) {
+		catalogContent.classList.remove('catalog_wrapper_show')
 		catalogContent.classList.add('catalog_wrapper_hide')
+	} else {
+		catalogContent.classList.add('catalog_wrapper_show')
+		catalogContent.classList.remove('catalog_wrapper_hide')
 	}
 })
