7e5cda66442fddfb9cae5e6f3d060c91cab4aadb
ulukmyrza2001 <ulukmyrzazhanybekov01@gmail.com>
2023-02-01 00:28:25 +0600

    feature cart

--- home/script.js
+++ home/script.js
@@ -1,4 +1,5 @@
 const catalogBtn = document.getElementById('catalog')
+const header = document.querySelector('.header')
 
 catalogBtn.addEventListener('click', () => {
 	const catalogContent = document.getElementById('catalog_wrapper')
@@ -8,5 +9,20 @@ catalogBtn.addEventListener('click', () => {
 	} else {
 		catalogContent.classList.add('catalog_wrapper_show')
 		catalogContent.classList.remove('catalog_wrapper_hide')
+	}
+})
+
+var shrinkHeader = 1
+
+function getCurrentScroll() {
+	return window.pageYOffset
+}
+
+window.addEventListener('scroll', () => {
+	var scroll = getCurrentScroll()
+	if (scroll >= shrinkHeader) {
+	   header.style.cssText = 'box-shadow: 3px 3px 9px rgba(0,0,0,0.2);'
+	} else {
+		header.style.cssText = 'box-shadow:none;'
 	}
 })
